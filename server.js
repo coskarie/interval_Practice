6f7af8830d0a40f2ac5dae2350d7d6c21ebedcc3
coskarie <vv8423@gmail.com>
2026-04-06 00:11:31 +0900

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// 현재 폴더의 파일들을 정적 파일로 서빙
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
